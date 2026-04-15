437924e3c8c3303ae48d0834df641f034cd77e54
AdityaKoda <adityakoda100@gmail.com>
2026-04-15 11:52:52 +0530

    ADITYA0001 - Added utility function

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,12 @@
+package com.example.javamavenjunithelloworld;
+
+public class Utility {
+
+    public static void agit add .ditya() {
+        System.out.println("I am Aditya");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
